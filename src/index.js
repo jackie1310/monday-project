9982195f983781ae5b8ba19d115b11866d6874de
jackie1310 <an.nguyenviet.13.10@gmail.com>
2023-12-29 12:42:46 +0000

    initial source

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
+
+// redux store
+import { Provider } from 'react-redux';
+import { store } from './reudux/configStore';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
