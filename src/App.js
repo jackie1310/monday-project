bf825d72dd347e35027c02028aab977f0cb6b9d6
jackie1310 <an.nguyenviet.13.10@gmail.com>
2023-12-29 15:54:54 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/create' Component={Role} />
+        <Route path='/create' Component={Role}/>
       </Routes>
     </>
   );
