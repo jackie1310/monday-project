53b02eeabb7cfca98f726ca00ed5b8b4ea06f4bf
jackie1310 <an.nguyenviet.13.10@gmail.com>
2024-01-05 08:47:48 +0000

    Update CreateTable.js

--- src/pages/CreateTable.js
+++ src/pages/CreateTable.js
@@ -5,5 +5,5 @@ import TableColumn from "./TableColumn/TableColumn";
 export default function CreateTable() {
     const [section, setSection] = useState(1);
     if (section === 1) return <BoardSetUp/>
-    if (section === 2) return <TableColumn/>
+    else if (section === 2) return <TableColumn/>
 }
