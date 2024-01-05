e19eb6857c46d1e0879c82e7c379b566424bfdb5
jackie1310 <an.nguyenviet.13.10@gmail.com>
2024-01-05 08:34:18 +0000

    Update App.js and Create.js

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import CreateAccount from "./CreateAccount/CreateAccount";
 import Role from "./Role";
+import Invites from "./Invites";
+import SocialAds from "./SocialAds";
 
 export default function Create() {
     const [section, setSection] = useState(1);
     if (section === 1) return <CreateAccount/>
     else if (section === 2) return <Role/>
+    else if (section === 3) return <Invites/>
+    else if (section == 4) return <SocialAds/>
 }
