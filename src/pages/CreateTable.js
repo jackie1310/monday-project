9bfaab3e69653d6d9d507d396edf1ca2841b7ad0
jackie1310 <an.nguyenviet.13.10@gmail.com>
2024-01-05 15:32:02 +0000

    Update CreateTable.js

--- src/pages/CreateTable.js
+++ src/pages/CreateTable.js
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import BoardSetUp from "./BoardSetUp/BoardSetUp";
 import TableColumn from "./TableColumn/TableColumn"
+import TableItem from "./TableItem/TableItems";
 
 export default function CreateTable() {
     const [section, setSection] = useState(1);
     if (section === 1) return <BoardSetUp setSection={setSection}/>
     else if (section === 2) return <TableColumn setSection={setSection}/>
+    else if (section === 3) return <TableItem setSection={setSection}/>
 }
