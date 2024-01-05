d252ea53d422c27c18edd7d789a670f52ae5cf13
jackie1310 <an.nguyenviet.13.10@gmail.com>
2024-01-05 13:21:32 +0000

    Update

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,38 @@
 import { Status } from "../data/Status";
 import { Month } from "../data/Month";
+import { Priority } from "../data/Prority";
+import { Timeline} from "../data/Timeline";
 
-export default function TableBody({rows, cols, className, setState, selectedOptions}) {
+export default function TableBody({rows, cols, className, setState, selectedOptions, item}) {
     function createBoard() {
         const table = [];
-        let numOfItem = 1
+        let numOfItem = 1;
+        const currentDate = new Date();
 
         for (let i = 0; i < rows; i++) {
             const row = [];
             if (selectedOptions?.length > 0) {
                 selectedOptions.map((option, index) => {
                     if (option === "Project") {
-                        row.push(<td key={index} className="text-left w-24 pl-2">Project {numOfItem}</td>)
+                        row.push(<td key={index} className="pr-4">{item} {numOfItem}</td>)
                         numOfItem++;
                     }
-                    if (option === "Owner") {
+                    else if (option === "Owner") {
                         row.push(<td key={index}>
                             <i className="fa-regular fa-user bg-black rounded-full p-2 text-white"></i>
                         </td>)
                     }
-                    if (option === "Status") {
+                    else if (option === "Status") {
                         row.push(<td key={index} className={`${Status[i].style} text-white w-fit`}>{Status[i].name}</td>)
                     }
-                    if (option === "Due date") {
-                        const currentDate = new Date();
-                        row.push(<td key={index}>{Month[currentDate.getMonth()]} {currentDate.getDate() + 1}</td>)
+                    else if (option === "Due date") {
+                        row.push(<td key={index}>{Month[currentDate.getMonth()]} {currentDate.getDate()}</td>)
+                    }
+                    else if (option === "Priority") {
+                        row.push(<td key={index} className={`${Priority[i].style} text-white`}>{Priority[i].name}</td>)
+                    }
+                    else if (option === "Timeline") {
+                        row.push(<td key={index} className="px-2"><p className={`${Timeline[i].style} text-white rounded-xl`}>{currentDate.getDate()} - {currentDate.getDate() + 1} {Month[currentDate.getMonth()]}</p></td>)
                     }
                 })
             } else {
