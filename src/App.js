4b8e21858da19ef3c2f7f79c457b62c558e372d9
jackie1310 <an.nguyenviet.13.10@gmail.com>
2024-01-05 08:33:29 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Create from './pages/Create';
 function App() {
   return (
     <>
-      <Routes></Routes>
+      <Routes>
+        <Route path='/' Component={SignUp}/>
+        <Route path='/create' Component={Create}/>
+      </Routes>
     </>
   );
 }
