49b26fe0ec995b96f89571220aefc0b509902b0c
jackie1310 <an.nguyenviet.13.10@gmail.com>
2024-01-08 07:39:04 +0000

    Update TableItems.jsx

--- src/page/TableItem/TableItems.jsx
+++ src/page/TableItem/TableItems.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react"
-import TableBody from "../../components/Table";
 import "./TableItem.scss";
-import Button from "../../components/Button";
 import { Radio } from "antd";
+import TableView from "../../components/ViewLayouts";
 
 export default function TableItem() {
-    const [boardName, setBoardName] = useState("");
     const [headers, setHeaders] = useState(3);
     const [rowsThisWeek, setRowsThisWeek] = useState(3);
     const [rowsNextWeek, setRowsNextWeek] = useState(1);
@@ -67,46 +65,7 @@ export default function TableItem() {
                 </div>
             </div>
             {/* Board */}
-            <div className="md:w-1/2 bg-gray-100 py-10 pl-5 h-screen">
-                <div className="flex flex-col w-full bg-white pl-5 py-4 md:mt-6 pl-1 shadow-2xl shadow-blue-300">
-
-                    <h1 className="text-2xl mb-10">My demo board</h1>
-                    <div className="w-fit mb-3">
-                        <div className="border-2 border-blue-500 rounded-lg"></div>
-                        <h2 className="text-xl">This week</h2>
-                    </div>
-                    
-                    <div className="flex flex-col border-l-4 rounded-lg border-blue-500 mb-10">
-                        <table className="flex-grow">
-                            <thead>
-                                <tr>
-                                    {createHeader()}
-                                    <th onClick={() => setHeaders(prev => prev + 1)} className="hover:cursor-pointer">+</th>
-                                </tr>
-                            </thead>
-                            <TableBody rows={rowsThisWeek} cols={selectedOptions?.length > 0 ? selectedOptions.length + 1 : headers + 1} setState={setRowsThisWeek} selectedOptions={selectedOptions} item={selectedItem}/>
-                        </table>
-                    </div>
-                    
-                    <div className="w-fit mb-3">
-                        <div className="border-2 border-green-500 rounded-lg"></div>
-                        <h2 className="text-xl">Next week</h2>
-                    </div>
-
-                    <div className="flex border-l-4 rounded-lg border-green-500">
-                        <table className="flex-grow">
-                            <thead>
-                                <tr>
-                                    {createHeader()}
-                                    <th onClick={() => setHeaders(prev => prev + 1)} className="hover:cursor-pointer">+</th>
-                                </tr>
-                            </thead>
-                            <TableBody rows={rowsNextWeek} cols={selectedOptions?.length > 0 ? selectedOptions.length + 1 : headers + 1} setState={setRowsNextWeek} selectedOptions={selectedOptions} item={selectedItem}/>
-                        </table>
-                    </div>
-                    
-                </div>
-            </div>
+            <TableView setHeaders={setHeaders} headers={headers} createHeader={createHeader} rowsThisWeek={rowsThisWeek} setRowsThisWeek={setRowsThisWeek} rowsNextWeek={rowsNextWeek} setRowsNextWeek={setRowsNextWeek} selectedOptions={selectedOptions} selectedItem={selectedItem}/>
         </div>
     )
 }
